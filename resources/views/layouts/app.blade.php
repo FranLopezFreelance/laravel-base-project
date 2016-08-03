238e3dbaf3ffd6dd28b3ab886ad32dcc5e55f1a7
Fran <franlopez.freelance@gmail.com>
2016-08-03 01:55:35 -0300

    Empezando con Pusher

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,5 +78,18 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+
+    <!-- PUSHER -->
+
+    <script src="//js.pusher.com/3.0/pusher.min.js"></script>
+    <script>
+        var pusher = new Pusher("{{ env("PUSHER_KEY") }}");
+        var channel = pusher.subscribe('test-chanel');
+
+        channel.bind('test-event', function(data){
+            alert(data.text);
+        });
+
+    </script>
 </body>
 </html>
